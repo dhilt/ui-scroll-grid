4b5fcba2a9d6c45dfaddc1c9fad774bec2c6fb1b
d.alexanov <d.alexanov@gmail.com>
2015-11-13 18:01:39 +0300

    columns refactoring

--- src/scripts/mainController.js
+++ src/scripts/mainController.js
@@ -3,6 +3,21 @@ angular.module('app').controller('mainController', [
 	function ($scope, console, $timeout, resource) {
 
 
+		// columns settings
+
+		$scope.columns = [
+			{ code: 'id', name: 'id', sortable: true, visible: true},
+			{ code: 'content', name: 'content', sortable: true, visible: true},
+			{ code: 'active', name: 'active', sortable: true, visible: true},
+			{ code: 'delete', name: 'delete', sortable: false, visible: true}
+		];
+
+		$scope.columnsObject = {};
+		for(var i = 0; i < $scope.columns.length; i++) {
+			$scope.columnsObject[$scope.columns[i].code] = $scope.columns[i];
+		};
+
+
 		// datasource implementation
 
 		$scope.datasource = {};
